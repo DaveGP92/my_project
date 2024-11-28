9406431ec3d0f3f38e972be47c7a75cfb36fe4aa
Dave <dave.gonzalez1992@outlook.com>
2024-11-27 20:16:39 -0500

    Configurando TailwindCSS en mi proyecto.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    // Agrega rutas a otros apps si es necesario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
