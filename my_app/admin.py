6e54ac743e13143533ec74b089ca24498b222409
Dave <dave.gonzalez1992@outlook.com>
2024-12-04 20:32:56 -0500

    Eliminando modelos para crear unos nuevos

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
 
-from . models import Customer
-
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-
-    fields = [field.name for field in Customer._meta.fields] # Registra todos los campos del modelo
-    fields = [field for field in fields if field not in ['deleted_at', 'updated_at', 'created_at']]
-    list_display = fields + ['updated_at', 'created_at', 'deleted_at']
     
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,2 @@
 from django.shortcuts import render
-from . models import Customer
-def first_view(request):
-    data = {
-        'customers':Customer.objects.all()
-    }
-    return render(request, 'index.html', data)
+
